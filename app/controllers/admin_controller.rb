5f4378d7f4582a92566290c0f42d80cdfd003958
Eric Martinez <eric.michael.mtz@gmail.com>
2017-11-21 11:36:30 -0600

    Fixed some bugs, made working admin role, added role_id to user

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,16 @@
 class AdminController < ApplicationController
+	before_action :authenticate_user!
+	before_action :admin?
+
 	def index
+	end
+
+
+	private
+
+	def admin?
+		if current_user.role_id != 99
+			redirect_to "/"
+		end
 	end
 end
